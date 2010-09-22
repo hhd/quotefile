f2e79d893546700848d9c5f1bf077fe277413e88
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-22 17:52:02 +1000

    hello world

---
+++ src/hello_www/core.clj
@@ -0,0 +1,12 @@
+(ns hello-www.core
+  (:use compojure.core
+        ring.adapter.jetty)
+  (:require [compojure.route :as route]))
+
+(defroutes example
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/not-found "Page not found"))
+
+(run-jetty example {:port 8080})
+
+
---
+++ test/hello_www/test/core.clj
@@ -0,0 +1,6 @@
+(ns hello-www.test.core
+  (:use [hello-www.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
