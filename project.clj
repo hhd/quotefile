c19382085bfcb9fd181bae4487ba05aacd04bb71
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-23 15:36:52 +1000

    fixed test directory name started using hiccup displaying some static test quotes

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject quotefile "1.0.0-SNAPSHOT"
-:description "A Compojure 'Hello World' application"
-:dependencies [[org.clojure/clojure "1.1.0"]
-               [org.clojure/clojure-contrib "1.1.0"]
-               [compojure "0.4.0"]
-               [ring/ring-jetty-adapter "0.2.3"]]
-:main quotefile.core)
+  :description "A Compojure 'Hello World' application"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [compojure "0.4.0"]
+                 [hiccup "0.2.6"]
+                 [ring/ring-jetty-adapter "0.2.3"]]
+  :main quotefile.core)
--- src/quotefile/core.clj
+++ src/quotefile/core.clj
@@ -1,10 +1,30 @@
 (ns quotefile.core
   (:use compojure.core
-        ring.adapter.jetty)
+        ring.adapter.jetty
+        hiccup.core)
   (:require [compojure.route :as route]))
 
+(def quotes [
+  "Sally: Google have some good search tools."
+  "Sally: I'm a retard. (totally without provocation while walking through a hacky sack game)"
+  "Logan: Celine! I've got balls on my site!
+   Sally: Are they in the back end?"
+  ])
+
+(defn list-quotes []
+  "Display the page that lists quotes"
+  (apply str (map (fn [quote] (html [:li quote])) quotes)))
+
+(defn html-outline [title body]
+    (html
+      [:html
+        [:head
+          [:title title]]
+        [:body body]]))
+
 (defroutes example
-  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (GET "/" [] (html-outline "Quotefile" (list-quotes)))
+  (GET "/:id" [id] (html [:h1 "Quote " id]))
   (route/not-found "Page not found"))
 
 (run-jetty example {:port 8080})
