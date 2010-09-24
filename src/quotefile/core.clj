83db0ee00d54590d8af522b3a49f21dc05b41ba7
Colin Campbell-McPherson <colin@hhd.com.au>
2010-09-24 15:58:22 +1000

    moved database logic into it's own namespace

--- src/quotefile/core.clj
+++ src/quotefile/core.clj
@@ -7,56 +7,9 @@
         hiccup.core
         hiccup.page-helpers)
   (:require [compojure.route :as route]
-            [clojure.contrib.sql :as sql]
-            [clojure.contrib.str-utils2 :as str])
-  (:import (java.sql DriverManager)))
+            [clojure.contrib.str-utils2 :as str]
+            [quotefile.database :as qdb]))
 
-(def quotes [
-  "Sally: Google have some good search tools."
-  "Sally: I'm a retard. (totally without provocation while walking through a hacky sack game)"
-  "Logan: Celine! I've got balls on my site!
-   Sally: Are they in the back end?"
-  ])
-
-;; Database stuff
-(def db {:classname "org.sqlite.JDBC"
-         :subprotocol "sqlite" ; Protocol to use
-         :subname "db/db.sqlite3" ; Location of db
-         :create true})
-
-(. Class (forName "org.sqlite.JDBC")) ; Initialize the JDBC driver
-
-(defn create-database
-  "Creates the table for this model"
-  []
-  (sql/with-connection
-   db
-   (sql/transaction
-    (sql/create-table
-     :quotes
-     [:id :integer "PRIMARY KEY AUTOINCREMENT"]
-     [:quote "text"]))))
-
-(defn insert-quote
-  "Inserts a quote into the database"
-  [quote]
-  (sql/with-connection db
-    (sql/insert-records "quotes"
-      {:quote quote})))
-
-(defn seed-database
-  "Seeds the database with a few quotes"
-  []
-  (map (fn [q] (insert-quote q)) quotes))
-
-(defn select-quotes
-  "Grabs all quotes from the database"
-  []
-  (sql/with-connection db
-    (sql/with-query-results results ["select * from quotes"]
-      (doall results))))
-
-;; Actual app
 (defn format-quote [quote]
   (map (fn [l] [:p l]) (str/split-lines quote)))
 
@@ -64,7 +17,7 @@
   "Display the page that lists quotes"
   (ordered-list
     (map (fn [quote] (format-quote (:quote quote)))
-         (select-quotes))))
+         (qdb/select-quotes))))
 
 (defn html-outline [title body]
     (html
@@ -75,7 +28,6 @@
 
 (defroutes handler
   (GET "/" [] (html-outline "Quotefile" (list-quotes)))
-  (GET "/:id" [id] (html-outline (str "Quote " id) (quotes (int id))))
   (route/not-found "Page not found"))
 
 (def app
---
+++ src/quotefile/database.clj
@@ -0,0 +1,50 @@
+(ns quotefile.database
+  (:use hiccup.core
+        hiccup.page-helpers)
+  (:require [clojure.contrib.sql :as sql])
+  (:import (java.sql DriverManager)))
+
+(def db {:classname "org.sqlite.JDBC"
+         :subprotocol "sqlite" ; Protocol to use
+         :subname "db/db.sqlite3" ; Location of db
+         :create true})
+
+(. Class (forName "org.sqlite.JDBC")) ; Initialize the JDBC driver
+
+            [clojure.contrib.str-utils2 :as str]
+(def quotes [
+  "Sally: Google have some good search tools."
+  "Sally: I'm a retard. (totally without provocation while walking through a hacky sack game)"
+  "Logan: Celine! I've got balls on my site!
+   Sally: Are they in the back end?"
+  ])
+
+(defn create-database
+  "Creates the table for this model"
+  []
+  (sql/with-connection
+   db
+   (sql/transaction
+    (sql/create-table
+     :quotes
+     [:id :integer "PRIMARY KEY AUTOINCREMENT"]
+     [:quote "text"]))))
+
+(defn insert-quote
+  "Inserts a quote into the database"
+  [quote]
+  (sql/with-connection db
+    (sql/insert-records "quotes"
+      {:quote quote})))
+
+(defn seed-database
+  "Seeds the database with a few quotes"
+  []
+  (map (fn [q] (insert-quote q)) quotes))
+
+(defn select-quotes
+  "Grabs all quotes from the database"
+  []
+  (sql/with-connection db
+    (sql/with-query-results results ["select * from quotes"]
+      (doall results))))
